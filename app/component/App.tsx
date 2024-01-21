64044698b45912e97ab588b4088525d7255649cb
EsanSamuel <esansamuel555@gmail.com>
2024-01-20 19:01:12 -0800

    new commits

--- app/component/App.tsx
+++ app/component/App.tsx
@@ -4,15 +4,15 @@ import React from 'react'
 const App = () => {
     return (
         <div className='md:p-20 p-5 py-20'>
-            <div className='md:flex justify-between gap-20'>
+            <div className='flex sm:flex-row flex-col justify-between gap-20'>
                 {appItems.map((item) => (
                     <div key={item.id}>
-                        <div className=' text-center'>
+                        <div className='sm:px-1 px-10 text-center'>
                             <div className='flex items-center justify-center'>
                                 <video src={item.video} className='w-[200px] h-[200px]' />
                             </div>
                             <div className='text-center'>
-                                <h1 className='md:text-[50px] text-[20px]'>{item.title}</h1>
+                                <h1 className='text-[50px]'>{item.title}</h1>
                                 <p className='md:text-[17px] text-[14px]'>{item.details}</p>
                             </div>
                         </div>
